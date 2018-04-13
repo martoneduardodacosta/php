751d41f21b08c413a251d9b9f23ddc5e25b6f536
Marton Eduardo da Costa <38335389+martoneduardodacosta@users.noreply.github.com>
2018-04-13 10:44:19 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+<<<<<<< HEAD
+echo "meu primeiro codigo php - <strong>Meu mundo 456</strong>";
+=======
+echo "meu primeiro codigo php - <strong>Meu mundo 123</strong>";
+echo "Ola Hcode
+
+?>
+
+
+
