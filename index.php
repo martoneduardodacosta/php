f4028ccc608fa58fca93e4941d1b06bdac2c231f
martoneduardodacosta <martoneduardodacosta@gmail.com>
2018-04-13 11:07:26 -0300

    ok

---
+++ index.php
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+<?php
+
+<<<<<<< HEAD
+echo "meu primeiro codigo php - <strong>Meu mundo 456</strong>";
+=======
+echo "meu primeiro codigo php - <strong>Meu mundo 123</strong>";
+echo "Ola Hcode
+
+?>
+
+
+
+=======
+<?php
+
+<<<<<<< HEAD
+echo "meu primeiro codigo php - <strong>Meu mundo 456</strong>";
+=======
+echo "meu primeiro codigo php - <strong>Meu mundo 123</strong>";
+echo "Ola Hcode
+
+?>
+
+
+
+>>>>>>> d5771d7457b3a04751196b9d0c4be900471b9032
