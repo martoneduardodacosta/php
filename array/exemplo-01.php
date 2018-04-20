7b9f515747f1dea44e2dce3159e00a7e245eef93
martoneduardodacosta <martoneduardodacosta@gmail.com>
2018-04-20 19:17:01 -0300

    ok

---
+++ array/exemplo-01.php
@@ -0,0 +1,8 @@
+<?php
+
+  $frutas = array("Laranja","Abacaxi", "Melancia");
+
+  print_r ($frutas);
+
+
+  ?>
---
+++ array/exemplo-02.php
@@ -0,0 +1,23 @@
+<?php
+
+    $carros  [0][0] = "GM";
+    $carros  [0][1] = "COBALT";
+    $carros  [0][2] = "ONIX";
+    $carros  [0][3] = "CAMARO";
+
+
+    $carros  [1][0] = "FORD";
+    $carros  [1][1] = "FIESTA";
+    $carros  [1][2] = "FUSION";
+    $carros  [1][3] = "ECO ESPORT";
+
+
+    echo $carros [0][3] . "<br>";
+
+    echo end($carros[1]);
+
+
+
+
+
+  ?>
